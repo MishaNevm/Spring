f57e65c58d90992c14683c313788bd29b22b9bbe
misha <ggvbgg@list.ru>
2022-11-20 18:52:22 +0300

    add properties

--- src/main/java/org/example/Ls3/Ls3.java
+++ src/main/java/org/example/Ls3/Ls3.java
@@ -5,7 +5,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Ls3 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        context.getBean("musicPlayer",MusicPlayer.class).playMusic();
+        MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
+        musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
         context.close();
     }
 }
--- src/main/java/org/example/Ls3/MusicPlayer.java
+++ src/main/java/org/example/Ls3/MusicPlayer.java
@@ -1,11 +1,30 @@
 package org.example.Ls3;
 
 public class MusicPlayer {
-    Music music;
+    private Music music;
+    private int volume;
+    private String name;
 
-    public MusicPlayer(Music music) {
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
     }
+
     public void playMusic () {
         System.out.println(music.getSong());
     }
