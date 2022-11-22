85be8534fb113c6efe3ccedf4067166bc2992c76
misha <ggvbgg@list.ru>
2022-11-22 22:05:05 +0300

    init method, destroy method and factory method

--- src/main/java/org/example/Ls3/Ls3.java
+++ src/main/java/org/example/Ls3/Ls3.java
@@ -5,12 +5,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Ls3 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayer",MusicPlayer.class);
-        MusicPlayer musicPlayer2 = context.getBean("musicPlayer",MusicPlayer.class);
-        musicPlayer1.setVolume(50);
-        musicPlayer2.setVolume(100);
-        System.out.println(musicPlayer1.getVolume());
-        System.out.println(musicPlayer2.getVolume());
+        Music music1 = context.getBean("musicBean",Music.class);
+        System.out.println(music1.getSong());
         context.close();
     }
 }
--- src/main/java/org/example/Ls3/RapMusic.java
+++ src/main/java/org/example/Ls3/RapMusic.java
@@ -1,8 +1,19 @@
 package org.example.Ls3;
 
 public class RapMusic implements Music{
+    private RapMusic() {}
     @Override
     public String getSong() {
         return "Yoyoyo it's a RAPchik";
+    }
+
+    public void doMyInit () {
+        System.out.println("Object created");
+    }
+    public void doMyDestroy () {
+        System.out.println("Object destory");
+    }
+    public static RapMusic createRapMusic() {
+        return new RapMusic();
     }
 }
